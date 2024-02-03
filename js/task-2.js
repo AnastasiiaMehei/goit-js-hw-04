32fc9f030cf8980b0611bbe84e36283b6436a96d
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-02-03 14:41:50 +0200

    2

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,19 @@
-// function calcAverageCalories(days) {}
+function calcAverageCalories(days) {
+  const values = Object.values(days);
+  let averageCalories = 0;
+  for (const value of values) {
+    if(value.)
+  }
+}
+
+console.log(
+  calcAverageCalories([
+    { day: 'monday', calories: 3010 },
+    { day: 'tuesday', calories: 3200 },
+    { day: 'wednesday', calories: 3120 },
+    { day: 'thursday', calories: 2900 },
+    { day: 'friday', calories: 3450 },
+    { day: 'saturday', calories: 3280 },
+    { day: 'sunday', calories: 3300 },
+  ])
+);
