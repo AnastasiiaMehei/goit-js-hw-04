2c83b55e0a0676ffdf7443829ba16cfee552f77a
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-02-03 17:28:37 +0200

    3

--- js/task-1.js
+++ js/task-1.js
@@ -6,7 +6,6 @@ function isEnoughCapacity(products, containerSize) {
   }
   return capacityRequired <= containerSize;
 }
-
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 
 console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,10 @@
 function calcAverageCalories(days) {
-  const values = Object.values(days);
-  let averageCalories = 0;
-  for (const value of values) {
-    if(value.)
+  let totalCalories = 0;
+  for (const day of days) {
+    totalCalories += day.calories;
   }
+  return totalCalories / days.length;
 }
-
 console.log(
   calcAverageCalories([
     { day: 'monday', calories: 3010 },
@@ -16,4 +15,18 @@ console.log(
     { day: 'saturday', calories: 3280 },
     { day: 'sunday', calories: 3300 },
   ])
-);
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: 'monday', calories: 2040 },
+    { day: 'tuesday', calories: 2270 },
+    { day: 'wednesday', calories: 2420 },
+    { day: 'thursday', calories: 1900 },
+    { day: 'friday', calories: 2370 },
+    { day: 'saturday', calories: 2280 },
+    { day: 'sunday', calories: 2610 },
+  ])
+); // 2270
+
+console.log(calcAverageCalories([])); // 0
